ad105f799b5bd87cb2e998e2b4363ae5e8faf8d9
Marco Montero <marco_montero1991@hotmail.com>
2016-06-22 21:57:02 -0600

    @dev added new approach to sieve, and added a description in c

--- src/Prime/prime.c
+++ src/Prime/prime.c
@@ -2,24 +2,24 @@
 Francisco Molina Castro, Marco Antonio Montero Chavarría
 This code generates prime numbers up to the first 5000 prime numbers  using
 eratostenes' sieve method
-/* to compile use g++ prime.cpp -o prime */
+/* to compile use gcc prime.c -o prime */
 /* to run use ./prime/
-/* to check execution time in unix type : time ./prime
+/* to check execution time in unix type : time ./prime */
 /**********************************************************/
-using namespace std;
-#include <iostream>
+# include <stdio.h>
+//# include <mpi.h>
 #include <stdio.h>
-#define N 48616 /* size of array to iterate on */
-# include <omp.h>
+# define N 48616 /* size of array to iterate on */
+//# include <omp.h>
 
 
 
 int main() {
-  # pragma omp parallel
-  int i, j, nums[N+1],  x = 1;
-  std::cout << "The first 5000 prime numbers are:\n" << std::endl;
 
-  # pragma omp for reduction ( + : total )
+  int i, j, nums[N+1],  x = 1;
+  printf("The first 5000 prime numbers are:\n");
+
+
   for( i = 2; i <= N; i ++ ){
       if( nums[i] != 1 ) {
         printf("%d,",i);
@@ -28,6 +28,5 @@ int main() {
         }
      }
 }
-  std::cout << "\n" << std::endl;
-  return 0;
+  printf("\n");
 }
